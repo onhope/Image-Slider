0c350acac83d71403d22b7d250f32a67e5556091
onhope <chch4934@gmail.com>
2023-10-23 18:27:23 +0900

    Image Slider

---
+++ index.js
@@ -0,0 +1,36 @@
+const nextEl = document.querySelector(".next");
+const prevEl = document.querySelector(".prev");
+
+const imgsEl = document.querySelectorAll("img");
+// console.log(imgsEl)
+const imageContaierEl = document.querySelector(".image-container");
+
+let currentImg = 1;
+let timeout;
+
+nextEl.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+prevEl.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+updateImg();
+
+function updateImg() {
+  if(currentImg > imgsEl.length) {
+    currentImg = 1;
+  } else if(currentImg < 1){
+    currentImg = imgsEl.length;
+  }
+  imageContaierEl.style.transform = `translateX(-${(currentImg - 1) * 500}px)`; 
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
+}
